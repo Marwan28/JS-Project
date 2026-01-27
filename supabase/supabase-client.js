82beb36c58893b69c6f44e6e81dbc9cf17914a0a
Eman Kamel <kamele432@gmail.com>
2026-01-27 10:55:35 +0200

    create structure of the project

---
+++ supabase/supabase-client.js
@@ -0,0 +1,3 @@
+const supabaseUrl = 'https://ujichqxxfsbgdjorkolz.supabase.co';
+const supabaseKey = 'sb_publishable_vs3dcyNAq9MoeQH77xkVuA_fGdHPIq6';
+const supabase = supabase.createClient(supabaseUrl, supabaseKey);
