cd7817131ef33d0021f43ed835c582f00aa1a5d0
MARWAN-PC\Marwan <marwanabdelwahab28@gmail.com>
2026-02-01 18:24:23 +0200

    merge login, signup, and main branches

---
+++ supabase/supabase_client.js
@@ -0,0 +1,4 @@
+import { createClient } from "https://esm.sh/@supabase/supabase-js";
+const supabaseUrl = 'https://ujichqxxfsbgdjorkolz.supabase.co';
+const supabaseKey = 'sb_publishable_vs3dcyNAq9MoeQH77xkVuA_fGdHPIq6';
+export const supabase = createClient(supabaseUrl, supabaseKey);
