ca1c441da5b6f24e08b84b673be6de559744e0fa
MARWAN-PC\Marwan <marwanabdelwahab28@gmail.com>
2026-02-04 02:46:38 +0200

    update stock quantity

--- admin/manage_order/manage_order.js
+++ admin/manage_order/manage_order.js
@@ -19,8 +19,6 @@ function loadFilteredOrders() {
   loadOrders(filteredOrders);
 }
 
-
-
 getAllOrders();
 async function getAllOrders() {
   try {
@@ -71,7 +69,7 @@ function loadOrders(orders) {
                 order_product.quantity * order_product.product.price;
               total += itemTotal;
               return `<div class="order_item">
-          <span>${order_product.product.name} x ${order_product.quantity}</span>
+          <span>${order_product.product.name} x ${order_product.quantity} <div>${order.status === "pending" ? "(In stock: " + order_product.product.stock_quantity + ")" : ""}</div></span>
           <span>$${itemTotal}</span>
           </div>`;
             })
@@ -83,10 +81,10 @@ function loadOrders(orders) {
               order.status === "pending"
                 ? `
         <div class="order_actions">
-          <button class="confirm_btn" onclick="updateOrderStatus(${order.id}, 'confirmed')">
+          <button class="confirm_btn" id="confirm_btn" data-order='${JSON.stringify(order)}' onclick="updateOrderStatus(this,'confirmed')">
             Confirm
           </button>
-          <button class="reject_btn" onclick="updateOrderStatus(${order.id}, 'rejected')">
+          <button class="reject_btn" id="reject_btn" data-order='${JSON.stringify(order)}' onclick="updateOrderStatus(this,'rejected')">
             Reject
           </button>
         </div>
@@ -99,10 +97,16 @@ function loadOrders(orders) {
     })
     .join("");
 }
-window.updateOrderStatus = async function (id, newStatus) {
+// var confirm_btn = document.getElementById("confirm_btn");
+// var reject_btn = document.getElementById("reject_btn");
+
+window.updateOrderStatus = async function (button, newStatus) {
+  const order = JSON.parse(button.dataset.order);
+  console.log(order);
+
   try {
     const response = await fetch(
-      `https://ujichqxxfsbgdjorkolz.supabase.co/rest/v1/orders?id=eq.${id}`,
+      `https://ujichqxxfsbgdjorkolz.supabase.co/rest/v1/orders?id=eq.${order.id}`,
       {
         method: "PATCH",
         headers: headers,
@@ -113,24 +117,44 @@ window.updateOrderStatus = async function (id, newStatus) {
     // const data = response.json();
     // console.log('data');
     // console.log(data);
-    
-    
 
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
+    console.log(`Order #${order.id} updated to ${newStatus}`);
+    try {
+      if (newStatus === "confirmed") {
+        order.order_products.forEach(async (element) => {
+          const old_stock = element.product.stock_quantity;
+          const quantity = element.quantity;
+          const new_stock = old_stock - quantity;
+          console.log(element.product.name);
+          console.log(element.product_id);
+          console.log(element.product.id);
+          console.log(old_stock);
+          console.log(quantity);
+          console.log(new_stock);
 
-    console.log(`Order #${id} updated to ${newStatus}`);
+          const res = await fetch(
+            `https://ujichqxxfsbgdjorkolz.supabase.co/rest/v1/product?id=eq.${element.product_id}`,
+            {
+              method: "PATCH",
+              headers: headers,
+              body: JSON.stringify({ stock_quantity: new_stock }),
+            },
+          );
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
 
-    statusFilter.value='all';
+    statusFilter.value = "all";
     getAllOrders();
-    
 
-    
-    alert(`Order #${id} ${newStatus} successfully!`);
+    // alert(`Order #${order.id} ${newStatus} successfully!`);
   } catch (error) {
     console.error("Error updating order:", error);
     alert("Failed to update order. Please try again.");
   }
 };
-
