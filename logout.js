365f1882ba8509bf93d61443a0db3b051ebbd906
MARWAN-PC\Marwan <marwanabdelwahab28@gmail.com>
2026-02-08 21:54:14 +0200

    fix paths

--- logout.js
+++ logout.js
@@ -1,7 +1,7 @@
-const BASE_URL = location.hostname.includes("github.io")
-  ? "/JS-Project/"
-  : "/";
 export function logout() {
+  const BASE_URL = location.hostname.includes("github.io")
+    ? "/JS-Project/"
+    : "/";
   localStorage.clear();
   window.location.replace(BASE_URL + "index.html");
   console.log("log out function");
